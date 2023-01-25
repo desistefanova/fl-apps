b32070bc46a7fd95e5b7977bd3dadebad482a7c9
Desislava Stefanova <dst.stefanova@gmail.com>
2023-01-25 15:02:40 +0200

    Migrate flutter_driver to integration_test

---
+++ beta_test_drive/integration_test/app_test.dart
@@ -0,0 +1,5 @@
+import '../test/widget_test.dart' as tests;
+
+void main() async {
+  tests.main();
+}
--- beta_test_drive/test_driver/app.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_driver/driver_extension.dart';
-import '../test/widget_test.dart' as tests;
-
-void main(List<String> args) async {
-  enableFlutterDriverExtension(handler: (command) async {
-    tests.main();
-    return "";
-  });
-}
